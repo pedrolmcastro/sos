34d2d4ca5740988bfc09f62694fe56d37c573874
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-09-05 21:57:25 -0300

    fix: correct include files for is_null and is_not_null in sos.hpp

--- include/sos/sos.hpp
+++ include/sos/sos.hpp
@@ -1,5 +1,5 @@
-#ifndef SOS_HPP
-#define SOS_HPP
+#ifndef SOS_SOS_HPP
+#define SOS_SOS_HPP
 
 
 #if __cplusplus < 202002L
@@ -10,12 +10,12 @@
 #include <sos/check/policy.hpp>
 #include <sos/check/checkable.hpp>
 
+#include <sos/predicates/is_null.hpp>
 #include <sos/predicates/is_empty.hpp>
 #include <sos/predicates/bool_cast.hpp>
 #include <sos/predicates/has_value.hpp>
 #include <sos/predicates/is_filled.hpp>
-#include <sos/predicates/is_equal_to_null.hpp>
-#include <sos/predicates/is_not_equal_to_null.hpp>
+#include <sos/predicates/is_not_null.hpp>
 
 #include <sos/traits/is_debug.hpp>
 #include <sos/traits/transparent.hpp>
