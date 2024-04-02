8708168c538d35bb35e6d9d4ae5932484314e469
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2024-04-02 01:21:45 -0300

    chore: add a parameter for verbose cmake in generate.sh

--- scripts/linux/generate.sh
+++ scripts/linux/generate.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 ROOT=$(dirname $0)/../..
-cmake ${ROOT} -B ${ROOT}/build -DSOS_BUILD_TESTS=${1:-OFF}
+cmake ${ROOT} -B ${ROOT}/build -DSOS_BUILD_TESTS=${1:-OFF} -DCMAKE_VERBOSE_MAKEFILE=${2:-OFF}
